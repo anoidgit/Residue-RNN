2ea83bae94b5e535537c6f3250af00cbc96f599c
ano <250081373@qq.com>
2016-04-21 10:23:30 +0800

    Create Residue Recurrent Neural Network.lua

---
+++ Residue Recurrent Neural Network.lua
@@ -0,0 +1,102 @@
+------------------------------------------------------------------------
+--ano build at 2016/04/21
+------------------------------------------------------------------------
+local ResidueRecurrent, parent = torch.class('nn.ResidueRecurrent', 'nn.Container')
+
+function ResidueRecurrent:__init(inid, input, nstate, rinput, rstate, merge, transfer, rho)
+	parent.__init(self)
+
+	self.statem1=inid["state-1"]
+	self.state0=inid["state0"]
+	self.inputm1=inid["input-1"]
+	self.input0=inid["input0"]
+	self.rho=rho
+	local parrelModel=nn.ParallelTable()
+		:add(input)
+		:add(nstate)
+		:add(rinput)
+		:add(rstate)
+	self.stateModel=nn.Sequential()
+		:add(parrelModel)
+		:add(merge)
+	self.outputModel=transfer
+end
+
+function ResidueRecurrent:forward(inputTable)
+	-- output(t) = transfer(state(t-1) + input(t) + state(t-2) + input(t-1))
+	local tmp
+	self.maxBPTT=(#inputTable<self.rho) and #inputTable or self.rho
+	self.output={}
+	inputTable[0]=self.input0
+	self.state={}
+	self.state[1]=self.statem1
+	self.state[2]=self.state0
+	for step=1,#inputTable do
+		tmp=self.stateModel:updateOutput({inputTable[step],self.state[step+1],inputTable[step-1],self.state[step]})
+		self.state[step+2]=tmp
+		self.output[step]=self.outputModel:updateOutput(tmp)
+	end
+	return self.output
+end
+
+function ResidueRecurrent:backward(inputTable, gradOutputTable, scale)
+	scale = scale or 1
+	local gradState,input,state_1,input_1,state_2
+	for step=#gradOutputTable,3,-1 do
+		gradState=self.outputModel:backward(self.state[step+2],gradOutputTable[step],scale)
+		input,state_1,input_1,state_2=unpack(self.stateModel:backward({inputTable[step],self.state[step+1],inputTable[step-1],self.state[step]},gradState))
+		gradOutputTable[step-1]+=state_1
+		gradOutputTable[step-2]+=state_2
+		self.gradInput[step]+=input
+		self.gradInput[step-1]=input_1
+	end
+	gradState=self.outputModel:backward(self.state[4],gradOutputTable[2],scale)
+	input,state_1,input_1,state_2=unpack(self.stateModel:backward({inputTable[2],self.state[3],inputTable[1],self.state[2]},gradState))
+	gradOutputTable[1]+=state_1
+	gradOutputTable[step-2]+=state_2--state 0 update here,step=2
+	self.gradInput[2]+=input
+	self.gradInput[1]=input_1
+	gradState=self.outputModel:backward(self.state[3],gradOutputTable[1],scale)
+	input,state_1,input_1,state_2=unpack(self.stateModel:backward({inputTable[1],self.state[2],self.input0,self.state[1]},gradState))
+	gradOutputTable[0]+=state_1--state 0 update here,step=1
+	gradOutputTable[-1]+=state_2--state -1 update here
+	self.gradInput[1]+=input
+	self.gradInput[0]=input_1--input 0 update here
+	return self.gradInput
+end
+
+function ResidueRecurrent:zeroGradParameters()
+	statenet:zeroGradParameters()
+	outputnet:zeroGradParameters()
+end
+
+function ResidueRecurrent:__tostring__()
+   local tab = '  '
+   local line = '\n'
+   local next = ' -> '
+   local str = torch.type(self)
+   str = str .. ' {' .. line .. tab .. '[{input(t), output(t-1)}'
+   for i=1,3 do
+      str = str .. next .. '(' .. i .. ')'
+   end
+   str = str .. next .. 'output(t)]'
+   
+   local tab = '  '
+   local line = '\n  '
+   local next = '  |`-> '
+   local ext = '  |    '
+   local last = '   ... -> '
+   str = str .. line ..  '(1): ' .. ' {' .. line .. tab .. 'input(t)'
+   str = str .. line .. tab .. next .. '(t==0): ' .. tostring(self.startModule):gsub('\n', '\n' .. tab .. ext)
+   str = str .. line .. tab .. next .. '(t~=0): ' .. tostring(self.inputModule):gsub('\n', '\n' .. tab .. ext)
+   str = str .. line .. tab .. 'output(t-1)'
+   str = str .. line .. tab .. next .. tostring(self.feedbackModule):gsub('\n', line .. tab .. ext)
+   str = str .. line .. "}"
+   local tab = '  '
+   local line = '\n'
+   local next = ' -> '
+   str = str .. line .. tab .. '(' .. 2 .. '): ' .. tostring(self.mergeModule):gsub(line, line .. tab)
+   str = str .. line .. tab .. '(' .. 3 .. '): ' .. tostring(self.transferModule):gsub(line, line .. tab)
+   str = str .. line .. '}'
+   return str
+end
