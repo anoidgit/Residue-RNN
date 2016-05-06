13a73c0d94c6390374dd39a9d397f5c1895cb08e
ano <250081373@qq.com>
2016-05-06 08:59:54 +0800

    Update FastResidueRecurrent.lua
    test pass

--- FastResidueRecurrent.lua
+++ FastResidueRecurrent.lua
@@ -1,15 +1,16 @@
 require "nn"
-------------------------------------------------------------------------
---ano build at 2016/04/21
-------------------------------------------------------------------------
-local FastResidueRecurrent, parent = torch.class('nn.FastResidueRecurrent', 'nn.Container')
+local FastResidueRecurrent, parent = torch.class('nn.FastResidueRecurrent', 'nn.Module')
 
 function FastResidueRecurrent:__init(inid, input, nstate, rinput, rstate, merge, transfer)
 	parent.__init(self)
-
+	self.output={}
+	self.state={}
 	self.statem1=inid["state-1"]
 	self.state0=inid["state0"]
 	self.input0=inid["input0"]
+	self.updstate0=self.statem1:clone():fill(0)
+	self.updstatem1=self.updstate0:clone()
+	self.updinput0=self.input0:clone():fill(0)
 	local parrelModel=nn.ParallelTable()
 		:add(input)
 		:add(nstate)
@@ -24,7 +25,7 @@ end
 function FastResidueRecurrent:forward(inputTable)
 	-- output(t) = transfer(state(t-1) + input(t) + state(t-2) + input(t-1))
 	local tmp
-	self.output={}
+	local outputTable={}
 	inputTable[0]=self.input0
 	self.state={}
 	self.state[1]=self.statem1
@@ -32,43 +33,60 @@ function FastResidueRecurrent:forward(inputTable)
 	for step=1,#inputTable do
 		tmp=self.stateModel:updateOutput({inputTable[step],self.state[step+1],inputTable[step-1],self.state[step]})
 		self.state[step+2]=tmp
-		self.output[step]=self.outputModel:updateOutput(tmp)
+		outputTable[step]=self.outputModel:updateOutput(tmp)
 	end
-	return self.output
+	return outputTable
 end
 
 function FastResidueRecurrent:backward(inputTable, gradOutputTable, scale)
 	scale = scale or 1
-	local gradState,input,state_1,input_1,state_2
-	for step=#gradOutputTable,3,-1 do
-		gradState=self.outputModel:backward(self.state[step+2],gradOutputTable[step],scale)
-		input,state_1,input_1,state_2=unpack(self.stateModel:backward({inputTable[step],self.state[step+1],inputTable[step-1],self.state[step]},gradState))
-		gradOutputTable[step-1]=gradOutputTable[step-1]+state_1
-		gradOutputTable[step-2]=gradOutputTable[step-2]+state_2
-		self.gradInput[step]=self.gradInput[step]+input
-		self.gradInput[step-1]=input_1
+	local input,state_1,input_1,state_2
+	local gradInput={}
+	local gradState={}
+	for step=#gradOutputTable,1,-1 do
+		gradState[step]=self.outputModel:backward(self.state[step+2],gradOutputTable[step],scale)
 	end
-	gradState=self.outputModel:backward(self.state[4],gradOutputTable[2],scale)
-	input,state_1,input_1,state_2=unpack(self.stateModel:backward({inputTable[2],self.state[3],inputTable[1],self.state[2]},gradState))
-	gradOutputTable[1]=gradOutputTable[1]+state_1
+	local cstep=#gradState
+	input,state_1,input_1,state_2=unpack(self.stateModel:backward({inputTable[cstep],self.state[cstep+1],inputTable[cstep-1],self.state[cstep]},gradState[cstep]))
+	gradState[cstep-1]:add(state_1)
+	gradState[cstep-2]:add(state_2)
+	gradInput[cstep]=input
+	gradInput[cstep-1]=input_1
+	for step=(#gradState-1),3,-1 do
+		input,state_1,input_1,state_2=unpack(self.stateModel:backward({inputTable[step],self.state[step+1],inputTable[step-1],self.state[step]},gradState[step]))
+		gradState[step-1]:add(state_1)
+		gradState[step-2]:add(state_2)
+		gradInput[step]:add(input)
+		gradInput[step-1]=input_1
+	end
+	input,state_1,input_1,state_2=unpack(self.stateModel:backward({inputTable[2],self.state[3],inputTable[1],self.state[2]},gradState[2]))
+	gradState[1]:add(state_1)
+	if (scale~=1) then
+		state_2:mul(scale)
+	end
 	self.updstate0=state_2--state 0 update here,step=2;gradOutputTable[step-2]+=state_2
-	self.gradInput[2]=self.gradInput[2]+input
-	self.gradInput[1]=input_1
-	gradState=self.outputModel:backward(self.state[3],gradOutputTable[1],scale)
-	input,state_1,input_1,state_2=unpack(self.stateModel:backward({inputTable[1],self.state[2],self.input0,self.state[1]},gradState))
-	self.updstate0=self.updstate0+state_1--state 0 update here,step=1;gradOutputTable[0]+=state_1
+	gradInput[2]:add(input)
+	gradInput[1]=input_1
+	input,state_1,input_1,state_2=unpack(self.stateModel:backward({inputTable[1],self.state[2],self.input0,self.state[1]},gradState[1]))
+	self.updstate0:add(scale,state_1)--state 0 update here,step=1;gradOutputTable[0]+=state_1
+	if (scale~=1) then
+		state_2:mul(scale)
+	end
 	self.updstatem1=state_2--state -1 update here;gradOutputTable[-1]+=state_2
-	self.gradInput[1]=self.gradInput[1]+input
-	self.updinput0=input_1--input 0 update here;self.gradInput[0]=input_1
-	return self.gradInput
+	gradInput[1]:add(input)
+	if (scale~=1) then
+		input_1:mul(scale)
+	end
+	self.updinput0=input_1--input 0 update here;gradInput[0]=input_1
+	return gradInput
 end
 
 function FastResidueRecurrent:zeroGradParameters()
 	self.stateModel:zeroGradParameters()
 	self.outputModel:zeroGradParameters()
-	self.updstate0=0
-	self.updstatem1=0
-	self.updinput0=0
+	self.updstate0:fill(0)
+	self.updstatem1:fill(0)
+	self.updinput0:fill(0)
 end
 
 function FastResidueRecurrent:updateParameters(learningRate)
@@ -80,5 +98,5 @@ function FastResidueRecurrent:updateParameters(learningRate)
 end
 
 function FastResidueRecurrent:__tostring__()
-	return torch.type(self)
+	return torch.type(self) .. "{\nOutput Module:" .. tostring(self.outputModel) .. "\nState Module:" ..  tostring(self.stateModel) .."\n}"
 end
