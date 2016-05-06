a4edc6ffe8e05b6f4e0343d4afb49cb2bb8eb5b9
ano <250081373@qq.com>
2016-05-06 19:17:31 +0800

    Update ClipGradientFastResidueRecurrent.lua
    seems test result pass

--- ClipGradientFastResidueRecurrent.lua
+++ ClipGradientFastResidueRecurrent.lua
@@ -4,6 +4,7 @@ local ClipGradientFastResidueRecurrent, parent = torch.class('nn.ClipGradientFas
 function ClipGradientFastResidueRecurrent:__init(inid, input, nstate, rinput, rstate, merge, transfer, maxgradient)
 	parent.__init(self)
 	self.output={}
+	self.gradInput={}
 	self.state={}
 	self.statem1=inid["state-1"]
 	self.state0=inid["state0"]
@@ -25,42 +26,38 @@ end
 
 function ClipGradientFastResidueRecurrent:forward(inputTable)
 	-- output(t) = transfer(state(t-1) + input(t) + state(t-2) + input(t-1))
-	local tmp
-	local outputTable={}
 	inputTable[0]=self.input0
 	self.state={}
 	self.state[1]=self.statem1
 	self.state[2]=self.state0
 	for step=1,#inputTable do
-		tmp=self.stateModel:updateOutput({inputTable[step],self.state[step+1],inputTable[step-1],self.state[step]})
-		self.state[step+2]=tmp
-		outputTable[step]=self.outputModel:updateOutput(tmp)
+		self.state[step+2]=self.stateModel:updateOutput({inputTable[step],self.state[step+1],inputTable[step-1],self.state[step]}):clone()
+		self.output[step]=self.outputModel:updateOutput(self.state[step+2]):clone()
 	end
-	return outputTable
+	return self.output
 end
 
 function ClipGradientFastResidueRecurrent:backward(inputTable, gradOutputTable, scale)
 	scale = scale or 1
 	local input,state_1,input_1,state_2
-	local gradInput={}
 	local gradState={}
 	for step=#gradOutputTable,1,-1 do
-		gradState[step]=self.outputModel:backward(self.state[step+2],gradOutputTable[step],scale)
+		gradState[step]=self.outputModel:backward(self.state[step+2],gradOutputTable[step],scale):clone()
 	end
 	local cstep=#gradState
 	gradState[cstep]:cmin(self.clipgradient)
 	input,state_1,input_1,state_2=unpack(self.stateModel:backward({inputTable[cstep],self.state[cstep+1],inputTable[cstep-1],self.state[cstep]},gradState[cstep]))
 	gradState[cstep-1]:add(state_1)
 	gradState[cstep-2]:add(state_2)
-	gradInput[cstep]=input
-	gradInput[cstep-1]=input_1
+	self.gradInput[cstep]=input:clone()
+	self.gradInput[cstep-1]=input_1:clone()
 	for step=(#gradState-1),3,-1 do
 		gradState[step]:cmin(self.clipgradient)
 		input,state_1,input_1,state_2=unpack(self.stateModel:backward({inputTable[step],self.state[step+1],inputTable[step-1],self.state[step]},gradState[step]))
 		gradState[step-1]:add(state_1)
 		gradState[step-2]:add(state_2)
-		gradInput[step]:add(input)
-		gradInput[step-1]=input_1
+		self.gradInput[step]:add(input)
+		self.gradInput[step-1]=input_1:clone()
 	end
 	gradState[2]:cmin(self.clipgradient)
 	input,state_1,input_1,state_2=unpack(self.stateModel:backward({inputTable[2],self.state[3],inputTable[1],self.state[2]},gradState[2]))
@@ -68,22 +65,22 @@ function ClipGradientFastResidueRecurrent:backward(inputTable, gradOutputTable, 
 	if (scale~=1) then
 		state_2:mul(scale)
 	end
-	self.updstate0=state_2--state 0 update here,step=2;gradOutputTable[step-2]+=state_2
-	gradInput[2]:add(input)
-	gradInput[1]=input_1
+	self.updstate0=state_2:clone()--state 0 update here,step=2;gradOutputTable[step-2]+=state_2
+	self.gradInput[2]:add(input)
+	self.gradInput[1]=input_1:clone()
 	gradState[1]:cmin(self.clipgradient)
 	input,state_1,input_1,state_2=unpack(self.stateModel:backward({inputTable[1],self.state[2],self.input0,self.state[1]},gradState[1]))
 	self.updstate0:add(scale,state_1)--state 0 update here,step=1;gradOutputTable[0]+=state_1
 	if (scale~=1) then
 		state_2:mul(scale)
 	end
-	self.updstatem1=state_2--state -1 update here;gradOutputTable[-1]+=state_2
-	gradInput[1]:add(input)
+	self.updstatem1=state_2:clone()--state -1 update here;gradOutputTable[-1]+=state_2
+	self.gradInput[1]:add(input)
 	if (scale~=1) then
 		input_1:mul(scale)
 	end
-	self.updinput0=input_1--input 0 update here;gradInput[0]=input_1
-	return gradInput
+	self.updinput0=input_1:clone()--input 0 update here;gradInput[0]=input_1
+	return self.gradInput
 end
 
 function ClipGradientFastResidueRecurrent:zeroGradParameters()
