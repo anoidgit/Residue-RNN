35170183665bcc2e3355086e04df34447a1f9371
ano <250081373@qq.com>
2016-05-07 17:54:06 +0800

    Update ClipGradientFastResidueRecurrent.lua

--- ClipGradientFastResidueRecurrent.lua
+++ ClipGradientFastResidueRecurrent.lua
@@ -6,12 +6,21 @@ function ClipGradientFastResidueRecurrent:__init(inid, input, nstate, rinput, rs
 	self.output={}
 	self.gradInput={}
 	self.state={}
-	self.statem1=inid["state-1"]
-	self.state0=inid["state0"]
-	self.input0=inid["input0"]
-	self.updstate0=self.statem1:clone():zero()
-	self.updstatem1=self.updstate0:clone()
-	self.updinput0=self.input0:clone():zero()
+	self.srcstatem1=inid["state-1"]
+	self.srcstate0=inid["state0"]
+	self.srcinput0=inid["input0"]
+	self.batchsize=1
+	self.statesize=self.srcstatem1:size()[2]
+	self.inputsize=self.srcinput0:size()[2]
+	self.statem1=self.srcstatem1
+	self.state0=self.srcstate0
+	self.input0=self.srcinput0
+	self.srcupdstate0=self.statem1:clone():zero()
+	self.srcupdstatem1=self.srcupdstate0:clone()
+	self.srcupdinput0=self.input0:clone():zero()
+	self.updstate0=self.srcupdstate0
+	self.updstatem1=self.srcupdstatem1
+	self.updinput0=self.srcupdinput0
 	self.clipgradient=maxgradient
 	local parrelModel=nn.ParallelTable()
 		:add(input)
@@ -27,13 +36,23 @@ end
 function ClipGradientFastResidueRecurrent:forward(inputTable)
 	-- output(t) = transfer(state(t-1) + input(t) + state(t-2) + input(t-1))
 	self.input=inputTable or self.input
+	local bsize=self.input[1]:size()[1]
+	if bsize~=self.batchsize then
+		self.batchsize=bsize
+		self.input0=self.srcinput0:expandAs(self.input[1])
+		self.updinput0=self.srcupdinput0:expandAs(self.input0)
+		self.statem1=self.srcstatem1:expand(self.batchsize,self.statesize)
+		self.state0=self.srcstate0:expandAs(self.statem1)
+		self.updstate0=self.srcupdstate0:expandAs(self.statem1)
+		self.updstatem1=self.srcupdstatem1:expandAs(self.statem1)
+	end
 	self.input[0]=self.input0
 	self.state={}
 	self.output={}
 	self.state[1]=self.statem1
 	self.state[2]=self.state0
 	for step=1,#self.input do
-		self.state[step+2]=self.stateModel:updateOutput({inputTable[step],self.state[step+1],inputTable[step-1],self.state[step]}):clone()
+		self.state[step+2]=self.stateModel:updateOutput({self.input[step],self.state[step+1],self.input[step-1],self.state[step]}):clone()
 		self.output[step]=self.outputModel:updateOutput(self.state[step+2]):clone()
 	end
 	return self.output
