736b1fd8231e83ba2efbd6fff3baf8a499bc1ecd
ano <250081373@qq.com>
2016-05-08 13:19:00 +0800

    Update ClipGradientFastResidueRecurrent.lua
    Support Sequential

--- ClipGradientFastResidueRecurrent.lua
+++ ClipGradientFastResidueRecurrent.lua
@@ -22,6 +22,7 @@ function ClipGradientFastResidueRecurrent:__init(inid, input, nstate, rinput, rs
 	self.updstatem1=self.srcupdstatem1
 	self.updinput0=self.srcupdinput0
 	self.clipgradient=maxgradient
+	self.train=true
 	local parrelModel=nn.ParallelTable()
 		:add(input)
 		:add(nstate)
@@ -31,6 +32,43 @@ function ClipGradientFastResidueRecurrent:__init(inid, input, nstate, rinput, rs
 		:add(parrelModel)
 		:add(merge)
 	self.outputModel=transfer
+end
+
+function ClipGradientFastResidueRecurrent:updateOutput(inputTable)
+	-- output(t) = transfer(state(t-1) + input(t) + state(t-2) + input(t-1))
+	self.input=inputTable or self.input
+	local bsize=self.input[1]:size()[1]
+	if bsize~=self.batchsize then
+		self.batchsize=bsize
+		self.input0=self.srcinput0:expandAs(self.input[1])
+		self.updinput0=self.srcupdinput0:expandAs(self.input0)
+		self.statem1=self.srcstatem1:expand(self.batchsize,self.statesize)
+		self.state0=self.srcstate0:expandAs(self.statem1)
+		self.updstate0=self.srcupdstate0:expandAs(self.statem1)
+		self.updstatem1=self.srcupdstatem1:expandAs(self.statem1)
+	end
+	self.input[0]=self.input0
+	self.state={}
+	self.output={}
+	if self.train then
+		self.state[1]=self.statem1
+		self.state[2]=self.state0
+		for step=1,#self.input do
+			self.state[step+2]=self.stateModel:updateOutput({self.input[step],self.state[step+1],self.input[step-1],self.state[step]}):clone()
+			self.output[step]=self.outputModel:updateOutput(self.state[step+2]):clone()
+		end
+	else
+		local evastatem1=self.statem1
+		local evastate0=self.state0
+		local evastate
+		for step=1,#self.input do
+			evastate=self.stateModel:updateOutput({self.input[step],evastate0,self.input[step-1],evastatem1}):clone()
+			self.output[step]=self.outputModel:updateOutput(evastate):clone()
+			evastatem1=evastate0
+			evastate0=evastate
+		end
+	end
+	return self.output
 end
 
 function ClipGradientFastResidueRecurrent:forward(inputTable)
@@ -49,17 +87,30 @@ function ClipGradientFastResidueRecurrent:forward(inputTable)
 	self.input[0]=self.input0
 	self.state={}
 	self.output={}
-	self.state[1]=self.statem1
-	self.state[2]=self.state0
-	for step=1,#self.input do
-		self.state[step+2]=self.stateModel:updateOutput({self.input[step],self.state[step+1],self.input[step-1],self.state[step]}):clone()
-		self.output[step]=self.outputModel:updateOutput(self.state[step+2]):clone()
+	if self.train then
+		self.state[1]=self.statem1
+		self.state[2]=self.state0
+		for step=1,#self.input do
+			self.state[step+2]=self.stateModel:updateOutput({self.input[step],self.state[step+1],self.input[step-1],self.state[step]}):clone()
+			self.output[step]=self.outputModel:updateOutput(self.state[step+2]):clone()
+		end
+	else
+		local evastatem1=self.statem1
+		local evastate0=self.state0
+		local evastate
+		for step=1,#self.input do
+			evastate=self.stateModel:updateOutput({self.input[step],evastate0,self.input[step-1],evastatem1}):clone()
+			self.output[step]=self.outputModel:updateOutput(evastate):clone()
+			evastatem1=evastate0
+			evastate0=evastate
+		end
 	end
 	return self.output
 end
 
 function ClipGradientFastResidueRecurrent:backward(inputTable, gradOutputTable, scale)
 	self.gradOutput = gradOutputTable or self.gradOutput
+	self.input = inputTable or self.input
 	scale = scale or 1
 	local input,state_1,input_1,state_2
 	local gradState={}
@@ -68,21 +119,21 @@ function ClipGradientFastResidueRecurrent:backward(inputTable, gradOutputTable, 
 	end
 	local cstep=#gradState
 	gradState[cstep]:cmin(self.clipgradient)
-	input,state_1,input_1,state_2=unpack(self.stateModel:backward({inputTable[cstep],self.state[cstep+1],inputTable[cstep-1],self.state[cstep]},gradState[cstep]))
+	input,state_1,input_1,state_2=unpack(self.stateModel:backward({self.input[cstep],self.state[cstep+1],self.input[cstep-1],self.state[cstep]},gradState[cstep]))
 	gradState[cstep-1]:add(state_1)
 	gradState[cstep-2]:add(state_2)
 	self.gradInput[cstep]=input:clone()
 	self.gradInput[cstep-1]=input_1:clone()
 	for step=(#gradState-1),3,-1 do
 		gradState[step]:cmin(self.clipgradient)
-		input,state_1,input_1,state_2=unpack(self.stateModel:backward({inputTable[step],self.state[step+1],inputTable[step-1],self.state[step]},gradState[step]))
+		input,state_1,input_1,state_2=unpack(self.stateModel:backward({self.input[step],self.state[step+1],self.input[step-1],self.state[step]},gradState[step]))
 		gradState[step-1]:add(state_1)
 		gradState[step-2]:add(state_2)
 		self.gradInput[step]:add(input)
 		self.gradInput[step-1]=input_1:clone()
 	end
 	gradState[2]:cmin(self.clipgradient)
-	input,state_1,input_1,state_2=unpack(self.stateModel:backward({inputTable[2],self.state[3],inputTable[1],self.state[2]},gradState[2]))
+	input,state_1,input_1,state_2=unpack(self.stateModel:backward({self.input[2],self.state[3],self.input[1],self.state[2]},gradState[2]))
 	gradState[1]:add(state_1)
 	if (scale~=1) then
 		state_2:mul(scale)
@@ -91,7 +142,7 @@ function ClipGradientFastResidueRecurrent:backward(inputTable, gradOutputTable, 
 	self.gradInput[2]:add(input)
 	self.gradInput[1]=input_1:clone()
 	gradState[1]:cmin(self.clipgradient)
-	input,state_1,input_1,state_2=unpack(self.stateModel:backward({inputTable[1],self.state[2],self.input0,self.state[1]},gradState[1]))
+	input,state_1,input_1,state_2=unpack(self.stateModel:backward({self.input[1],self.state[2],self.input0,self.state[1]},gradState[1]))
 	self.updstate0:add(scale,state_1)--state 0 update here,step=1;self.gradOutput[0]+=state_1
 	if (scale~=1) then
 		state_2:mul(scale)
@@ -119,6 +170,14 @@ function ClipGradientFastResidueRecurrent:updateParameters(learningRate)
 	self.state0:add(-learningRate,self.updstate0)
 	self.statem1:add(-learningRate,self.updstatem1)
 	self.input0:add(-learningRate,self.updinput0)
+end
+
+function ClipGradientFastResidueRecurrent:training()
+	self.train=true
+end
+
+function ClipGradientFastResidueRecurrent:evaluate()
+	self.train=false
 end
 
 function ClipGradientFastResidueRecurrent:__tostring__()
